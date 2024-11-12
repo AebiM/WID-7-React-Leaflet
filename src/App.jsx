e31a975160deef468457c1124d810cfd801b3f50
AebiM <manuel.aebi@students.fhnw.ch>
2024-11-12 13:35:07 +0100

    fertig

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import Typography from "@mui/material/Typography";
+// import Typography from "@mui/material/Typography";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { Marker } from "react-leaflet";
+import { Popup } from "react-leaflet";
+
+// import { useMap } from 'react-leaflet/hooks'
 
 function App() {
+  const position = [47.53482609456414, 7.6419273263886875];
+
   return (
-    <div>
-      <Typography variant="h2">Hello React & MUI</Typography>
-    </div>
+    <MapContainer
+      style={{ height: "100vh" }}
+      center={position}
+      zoom={18}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
